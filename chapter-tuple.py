2d10e1436b7b3e5d20d2385e56192a23d7f4fcc0
sky81219 <=>
2022-12-17 02:29:39 +0900

    modify

---
+++ chapter-tuple.py
@@ -0,0 +1,27 @@
+# lax_coordinates = (33.9425, -118.408056)
+# city, year, pop, chg, area = ("Tokyo", 2003, 32540, 0.66, 8014)
+# traveler_ids = [("USA", '31195855'), ("BRA", "CE342567"), ("ESP", "XDA205856")]
+
+# for passport in sorted(traveler_ids):
+#     print('%s / %s' % passport)
+    
+# for country, _ in traveler_ids:
+#     print(country)
+    
+    
+
+# 비파과적 파괴적 
+# lax_coordinates = (33.9425, -118.408056)
+# a, b = lax_coordinates
+# print(lax_coordinates)
+
+# a = [1, 2, 3, 4, 5, 6]
+# b = [7, 8, 9]
+
+# a.extend(b)
+# print(a)
+# print(b)
+
+t = (20, 8)
+a, b = divmod(*t)
+print(a, b)
